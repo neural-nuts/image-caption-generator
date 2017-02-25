786810f3b8efe371b76c8234e5f4098dd3ffa314
Pranay Mathur <pranay360@gmail.com>
2017-02-25 19:10:23 +0530

    Update generate_data.py

--- ImgCap/generate_data.py
+++ ImgCap/generate_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 from collections import Counter
 from nltk.tokenize import word_tokenize
 import pickle
+import os
 
 max_len = 20
 word_threshold = 2
@@ -23,7 +24,7 @@ def preprocess_captions(filenames, captions):
 
 
 def generate_vocab(df):
-    global max_len, word_threshold
+    global max_len, word_threshold, counter
     print "Generating Vocabulary"
 
     vocab = dict([w for w in counter.items() if w[1] >= word_threshold])
@@ -37,15 +38,17 @@ def generate_vocab(df):
     wtoidx["<PAD>"] = 0
     wtoidx["<UNK>"] = 3
     print "Generating Word to Index and Index to Word"
-    for i, word in enumerate(vocab.keys()):
+    i = 4
+    for word in vocab.keys():
         if word not in ["<S>", "</S>", "<PAD>", "<UNK>"]:
-            wtoidx[word] = i + 4
+            wtoidx[word] = i
+            i+=1
     print "Size of Vocabulary", len(vocab)
     return vocab, wtoidx
 
 
 def pad_captions(df):
-    global max_len, counter
+    global max_len
     print "Padding Caption <PAD> to Max Length", max_len, "+ 2 for <S> and </S>"
     dfPadded = df.copy()
     dfPadded['caption'] = "<S> " + dfPadded['caption'] + " </S>"
@@ -58,8 +61,6 @@ def pad_captions(df):
             pad_buf = "<PAD> " * pad_len
             pad_buf = pad_buf.strip()
             dfPadded.set_value(i, 'caption', cap + " " + pad_buf)
-    print "Saving Padded Captions to paddedcaptions.csv"
-    #dfPadded.to_csv('Dataset/paddedcaptions.csv',index = False)
     return dfPadded
 
 
@@ -69,13 +70,39 @@ def load_features(feature_path):
     print "Features Loaded", feature_path
     return features
 
+def split_dataset(df, features, ratio=0.8):
+    split_idx = int(df.shape[0]*ratio)
+    print "Data Statistics:"
+    print "# Records Total Data: ", df.shape[0]
+    print "# Records Training Data: ", split_idx
+    print "# Records Training Data: ", df.shape[0]-split_idx
+    print "Ration of Training: Validation = ", ratio*100, ":", 100-(ratio*100)
+    val_features = features[split_idx:]
+    val_captions = np.array(df.caption)[split_idx:]
+    np.save("Dataset/Validation_Data",zip(val_features, val_captions))
+    return df[:split_idx], features[:split_idx]
+
+def get_data(required_files):
+    ret=[]
+    for fil in required_files[:-1]:
+        ret.append(np.load("Dataset/"+fil+".npy"))
+    return ret
 
 def generate_captions(
         wt=2,
         ml=20,
         cap_path='Dataset/results_20130124.token',
-        feat_path='Dataset/features.npy',
-        mode='train'):
+        feat_path='Dataset/features.npy'):
+    required_files = ["vocab", "wordmap", "Training_Data", "Validation_Data"]
+    generate = False
+    for fil in required_files:
+        if not os.path.isfile('Dataset/'+fil+".npy"):
+            generate = True
+            print "Required Files not present. Regenerating Data."
+            break
+    if generate == False:
+        print "Dataset Present; Skipping Generation."
+        return get_data(required_files)
     global max_len, word_threshold, counter
     max_len = ml
     word_threshold = wt
@@ -85,15 +112,21 @@ def generate_captions(
     filenames = [caps.split('\t')[0].split('#')[0] for caps in data]
     captions = [caps.replace('\n', '').split('\t')[1] for caps in data]
     df = preprocess_captions(filenames, captions)
+    features = load_features(feat_path)
+    idx = np.random.permutation(features.shape[0])
+    df=df.iloc[idx]
+    features=features[idx]
+    df, features = split_dataset(df, features)
     counter = Counter()
     for i, row in df.iterrows():
         counter.update(row["caption"].lower().split())
-    print len(counter)
     df = pad_captions(df)
     vocab, wtoidx = generate_vocab(df)
-    if mode is 'test':
-        return vocab, wtoidx
-    features = load_features(feat_path)
     captions = np.array(df.caption)
+    np.save("Dataset/Training_Data",zip(features,captions))
+    np.save("Dataset/wordmap", wtoidx)
+    np.save("Dataset/vocab", vocab)
+
     print "Preprocessing Complete"
-    return vocab, wtoidx, features, captions
+    return get_data(required_files)
+
